7da4f3105a4c46a6eab50c7e3a81266f1d9ffe2f
Muhammad Saad <muhammad.saad@10pearls.com>
2023-02-28 15:39:33 +0500

    first commit

---
+++ practice.py
@@ -0,0 +1,24 @@
+a = 1
+b = 2
+string = "name"
+lists = [1, "a", 2, "b", "c", 3.3, "d", 5.0]
+tup = ("test", "senior", "staff")
+
+print(lists)
+print(tup)
+print(lists[6])
+print(tup[2])
+
+lists[0] = "a"
+lists[1] = "1"
+lists[2] = "b"
+lists[3] = "2"
+print(lists)
+
+lists.remove('b')
+lists.append('b')
+print(lists)
+
+dict = {"ide": "Pycharm", "lang": "Python", "Session": 1}
+dict["Session"] = 2
+print(dict)
